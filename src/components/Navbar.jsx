d5a2137b90342e289c9b69cac56532db4fc90da9
Vrivas99 <vi.rivas@duocuc.cl>
2024-11-12 02:13:56 -0300

    fix nav responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,21 +11,21 @@ const Navbar = () => {
 
 
     return (
-        <nav className="fixed z-20 right-10 top-1/2 transform -translate-y-1/2 gap-7 flex flex-col">
+        <nav className="fixed z-20 bottom-5 gap-3 right-1/2 transform translate-x-1/2 md:right-10 md:top-1/2 md:-translate-y-1/2 md:gap-7 flex md:flex-col">
             <NavBtn href="#Home" tooltip="Inicio">
-                <Home />
+                <Home width={24}/>
             </NavBtn>
             <NavBtn href="#Experience" tooltip="Experiencia">
-                <Work />
+                <Work width={24} height={24}/>
             </NavBtn>
             <NavBtn href="#Projects" tooltip="Proyectos">
-                <Code />
+                <Code width={24} height={24}/>
             </NavBtn>
             <NavBtn href="#" tooltip="Yo">
-                <About />
+                <About width={24} height={24}/>
             </NavBtn>
             <NavBtn href="#" tooltip="Contacto">
-                <Contact />
+                <Contact width={24} height={24}/>
             </NavBtn>
         </nav>
     )
